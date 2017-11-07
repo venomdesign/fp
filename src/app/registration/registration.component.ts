675a58172c6cbadfa54ef88fa897040d811e7fd1
Chris Schweigart <chris.schweigart@venomdesign.com>
2017-11-07 16:54:07 -0500

    comments

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -53,11 +53,39 @@ firstName: new FormControl('', [
               Validators.pattern('[a-zA-Z ]*'),
               Validators.required
               ]),
+          middleName: new FormControl('', [
+              Validators.maxLength(1), 
+              Validators.pattern('[a-zA-Z]*'), 
+              Validators.required]),              
           lastName: new FormControl('', [
               Validators.maxLength(50), 
               Validators.pattern('[a-zA-Z ]*'), 
               Validators.required]),
-      email: new FormControl('', [
+          companyId: new FormControl('', [
+              Validators.maxLength(50), 
+              Validators.required]),
+          phoneNumber: new FormControl('', [
+              Validators.maxLength(14), 
+              Validators.required]),
+          phoneExt: new FormControl('', [
+              Validators.maxLength(5), 
+              Validators.required]),
+          mobileNumber: new FormControl('', [
+              Validators.maxLength(14), 
+              Validators.required]),
+          timeZone: new FormControl('', [Validators.required])
+*/
+
+   /*       block all special characters and copy and paste from anything other than text based editor, strip all special characters
+          comment: new FormControl('', [
+              Validators.maxLength(5), 
+              Validators.required]),
+          mFaEnabled: new FormControl('', [ Validators.required])
+          Mobile NUmber only required qhen slecting YES*/
+
+
+
+/*email: new FormControl('', [
           Validators.maxLength(50), 
           EmailValidator.isValidMailFormat, 
           Validators.required]),
@@ -66,5 +94,4 @@ firstName: new FormControl('', [
               Validators.required]),
           confirmPassword: new FormControl('', [
               Validators.maxLength(50), 
-              Validators.required])
-*/
+              Validators.required]),*/
--- src/app/shared/validation/forms.ts
+++ src/app/shared/validation/forms.ts
@@ -2,7 +2,7 @@ import {FormControl} from '@angular/forms';
 
 export class EmailValidator {
    static isValidMailFormat(control: FormControl){
-        let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+        let EMAIL_REGEXP = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
 
         if (control.value != "" && (control.value.length <= 5 || !EMAIL_REGEXP.test(control.value))) {
             return { "Please provide a valid email": true };
